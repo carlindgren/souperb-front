ff1dfe30179bb6367fce18dc351bc6588f18c956
Carl Lindgren <carlindgren@outlook.com>
2020-11-01 22:39:17 +0100

    hosting

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const path = require('path');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 // set up express
 const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
 
 app.use(express.json());
 app.use(cors());
-
+app.use(express.static(publicPath));
 //set up mongoose
 const uri = process.env.MONGO_URI;
 mongoose.connect(uri, {
@@ -31,6 +32,10 @@ app.use('/users', require('./routes/userRouter'));
 app.use('/breads', require('./routes/breadRouter'));
 app.use('/drinks', require('./routes/drinkRouter'));
 app.use('/soups', require('./routes/soupRouter'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
 
 app.listen(port, () => {
   console.log('app running on port: ' + port);
